2abb8dfe9c38b96fef625ff12c3b8632d841b930
Rafał Janus <rafal.janus.94@gmail.com>
2019-03-06 15:36:24 +0100

    Development dependency addded - Styled-Componenets showing names now

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,15 @@
 module.exports = {
   pathPrefix: "/cv-resume",
   plugins: [
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -9,7 +17,5 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
   ],
 }
